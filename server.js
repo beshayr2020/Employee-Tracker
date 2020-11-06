161989f1e862794be9884b0b4b5011e9031dffc5
Beshayr <beshayr37@gmail.com>
2020-11-05 18:40:50 -0600

    updating comments server.js file

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const mysql = require('mysql');
 const inquirer = require('inquirer');
 const chalk = require('chalk');
 const cTable = require('console.table');
-//const connection = require('./config/connection')
 const figlet = require('figlet');
 const startScreen = ['View all Employees', 'View all Emplyees by Department', 'View all Employees by Manager', 'Add Employee', 'Remove Employee', 'Update Employee Role', 'View all Roles', 'Add Role', 'Remove Role', 'View all Departments', 'Add Department', 'Remove Department', 'Exit']
 const allEmployeeQuery = `SELECT e.id, e.first_name AS "First Name", e.last_name AS "Last Name", r.title, d.department_name AS "Department", IFNULL(r.salary, 'No Data') AS "Salary", CONCAT(m.first_name," ",m.last_name) AS "Manager"
